afa10705726005b95c58b5ef7a3efbcf7e92f419
Nicholas Walker <nwalk7800@gmail.com>
2023-12-04 14:20:07 -0600

    Day 4

---
+++ Day4Part1.ps1
@@ -0,0 +1,15 @@
+﻿$Sum = 0
+
+foreach ($Card in Get-Clipboard) {
+    $Game, $Winning, $Mine = $Card.Trim().split(':\|')
+    $WinNumbers = $Winning.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)
+    $MyNumbers = $Mine.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)
+
+    $Hits = $MyNumbers | ?{$_ -in $WinNumbers}
+
+    if ($hits.Count -gt 0) {
+        $Sum += [Math]::Pow(2, $Hits.Count-1)
+    }
+}
+
+$Sum
---
+++ Day4Part2.ps1
@@ -0,0 +1,45 @@
+﻿
+function SumCards {
+    param (
+        $Cards
+    )
+    
+    $CardsTotal = 0
+    
+    foreach ($Card in $Cards) {
+        $CardTotal = 0
+        
+        if ($SavedCards.ContainsKey($Card.GameNum)) {
+            $CardTotal = $SavedCards[$Card.GameNum]
+        } else {
+            $Hits = @($Card.MyNumbers | ?{$_ -in $Card.WinNumbers})
+
+            if ($Hits.Length -gt 0) {
+                $CardTotal += $Hits.Length
+                $CardTotal += SumCards $AllCards[$Card.GameNum..($Card.GameNum+$Hits.Length-1)]
+            }
+            $SavedCards[$Card.GameNum] = $CardTotal
+        }
+        $CardsTotal += $CardTotal
+    }
+    $CardsTotal
+}
+
+$AllCards = Get-Clipboard | %{
+    $Game, $Winning, $Mine = $_.Trim().split(':\|')
+    
+    $Retval = "" | select GameNum, WinNumbers, MyNumbers
+    [Int]$Retval.GameNum = $Game.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)[1]
+    $Retval.WinNumbers = $Winning.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)
+    $Retval.MyNumbers = $Mine.Split(" ", [StringSplitOptions]::RemoveEmptyEntries)
+
+    $Retval
+}
+
+$SavedCards = @{}
+
+$Sum = $AllCards.Length
+
+$Sum += SumCards $AllCards
+
+$Sum
