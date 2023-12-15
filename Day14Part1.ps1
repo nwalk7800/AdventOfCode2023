a47838abc31883ed7d4047fed5acbdaa4c95bad9
Nicholas Walker <nwalk7800@gmail.com>
2023-12-15 09:23:04 -0600

    Day 14 part 1

---
+++ Day14Part1.ps1
@@ -0,0 +1,48 @@
+﻿$Maps = ((Get-Clipboard) -join "`n") -split "`n`n"
+
+function TiltNorth {
+    param (
+        $Map
+    )
+
+    do {
+        $Change = $false
+        for ($Row = 1; $Row -lt $Map.Count; $Row++) {
+            for ($Col = 0; $Col -lt $Map[$Row].Count; $Col++) {
+                if ($Map[$Row][$Col] -eq "O" -and $Map[$Row-1][$Col] -eq ".") {
+                    $Map[$Row][$Col] = "."
+                    $Map[$Row-1][$Col] = "O"
+                    $Change = $true
+                }
+            }
+        }
+    } while ($Change)
+}
+
+function GetLoad {
+    param (
+        $Map
+    )
+
+    $Load = 0
+    for ($Row = 0; $Row -lt $Map.Count; $Row++) {
+        for ($Col = 0; $Col -lt $Map[$Row].Count; $Col++) {
+            if ($Map[$Row][$Col] -eq "O") {
+                $Load += $Map.Count - $Row
+            }
+        }
+    }
+    $Load
+}
+
+foreach ($MapText in $Maps) {
+    $Map = @()
+    foreach ($Row in $MapText -split "`n") {
+        $Map += ,$Row.ToCharArray() 
+    }
+
+    TiltNorth $Map
+    $Load = GetLoad $Map
+}
+
+$Load
